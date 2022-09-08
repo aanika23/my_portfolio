a5fafda99598aaf74442da502bbe79f58ecd8b2b
unknown <anikaantara95@gmail.com>
2022-09-07 22:37:27 -0700

    started animations on image body

--- my-portfolio/src/components/image_body.jsx
+++ my-portfolio/src/components/image_body.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import profilePic from './image/profile_pic_no_bg.png'
+import {motion} from 'framer-motion'
 
 class Image_body extends Component {
     state = { 
-        imageBodyUrl: './image/profile_pic_no_bg.png'
      } 
     render() { 
         return (
             <div>
-                <img src={profilePic} id="image_mov"></img>
+                <motion.div animate={{x: 600}} transition={{duration:2}} id="intro_text_box">
+                    <p class="intro_text m-2">HI THERE. I'M</p>
+                    <h1 id="intro_text_header" class="intro_text">Hire Me Please</h1>
+                    <p class="intro_text">Recent Software/Computer Engineer Grad</p>
+                </motion.div>
+                <motion.img animate={{x: -600}} transition={{duration:2}} src={profilePic} id="image_mov"></motion.img>
             </div>
         );
     }
