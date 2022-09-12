752fefea0390fe2a83288a9c0963b9d703b3851b
unknown <anikaantara95@gmail.com>
2022-09-11 22:04:27 -0700

    added bg pic

--- my-portfolio/src/components/main.jsx
+++ my-portfolio/src/components/main.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Image_body from './image_body';
 import Navbar from './navbar';
+import './css/home.css';
 
 export default class Main extends Component {
     state = {  } 
     render() { 
         return (
-           <div>
+           <div id="container">
                 <Navbar />
            </div>
         );
--- my-portfolio/src/components/navbar.jsx
+++ my-portfolio/src/components/navbar.jsx
@@ -18,12 +18,12 @@ export default class Navbar extends Component {
             <nav className='nav'>
                 <a href='#' className='nav__logo'>Anika</a>
                 <ul className={this.state.active}>
-                    <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
-                    <li><a href="#" className="nav__link">About Me</a></li>
-                    <li><a href="#" className="nav__link">Resume</a></li>
-                    <li><a href="#" className="nav__link">Skills</a></li>
-                    <li><a href="#" className="nav__link">Projects</a></li>
-                    <li><a href="#" className="nav__link">Contacts</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">Home</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">About Me</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">Resume</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">Skills</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">Projects</a></li>
+                    <li className="nav__link__box"><a href="#" className="nav__link">Contacts</a></li>
                 </ul>
                 <div onClick = {() => this.navToggle()} className="nav__toggler">
                     <div className="line1"></div>
