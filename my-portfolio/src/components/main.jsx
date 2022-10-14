432f17c1dd192a9f3b26a30d74b2591fd016b14a
unknown <anikaantara95@gmail.com>
2022-10-14 15:29:28 -0700

    started routing

--- my-portfolio/src/components/main.jsx
+++ my-portfolio/src/components/main.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from './navbar';
 import Home_animation from './Home_animation';
+import AboutMe from './about_me';
 import './css/home.css';
 import "@fontsource/amatic-sc";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 export default class Main extends Component {
     state = {  } 
     render() { 
         return (
-           <div id="container">
-                <Navbar />
-                <Home_animation />
-           </div>
+            <BrowserRouter>
+                <div id="container">
+                    <Navbar />
+                    <Routes>
+                        <Route path="/about_me" element={<AboutMe/>} />
+                        <Route path="/" element={<Home_animation/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+           
         );
     }
 }
